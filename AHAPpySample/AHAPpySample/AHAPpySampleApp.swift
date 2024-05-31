0bc1e9e61c788495aeb93c868ce63f575ad43737
Samuel Roman <samroman3@gmail.com>
2024-05-31 16:19:30 -0400

    add views, cleanup

---
+++ AHAPpySample/AHAPpySample/AHAPpySampleApp.swift
@@ -0,0 +1,8 @@
+//
+//  AHAPpySampleApp.swift
+//  AHAPpySample
+//
+//  Created by Sam Roman on 5/31/24.
+//
+
+import Foundation
---
+++ AHAPpySample/AHAPpySample/ContentView.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  AHAPpySample
+//
+//  Created by Sam Roman on 5/31/24.
+//
+
+import Foundation
